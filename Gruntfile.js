bd42dbc272364549fb8942e91fd08ee667085257
R.G. Wood <ric@grislyeye.com>
2016-05-14 17:52:47 +0100

    Initial skeleton document
    Created initial skeleton HTML-based document with CSS layout and
    publish to PDF.
    
    Current PDF export does not support Flexbox.

---
+++ Gruntfile.js
@@ -0,0 +1,189 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  var stylelintConfig = {
+    "rules": {
+      "block-no-empty": true,
+      "color-no-invalid-hex": true,
+      "declaration-colon-space-after": "always",
+      "declaration-colon-space-before": "never",
+      "function-comma-space-after": "always",
+      "function-url-quotes": "double",
+      "media-feature-colon-space-after": "always",
+      "media-feature-colon-space-before": "never",
+      "media-feature-name-no-vendor-prefix": true,
+      "max-empty-lines": 5,
+      "number-leading-zero": "never",
+      "number-no-trailing-zeros": true,
+      "property-no-vendor-prefix": true,
+      "rule-no-duplicate-properties": true,
+      "declaration-block-no-single-line": true,
+      "rule-trailing-semicolon": "always",
+      "selector-list-comma-space-before": "never",
+      "selector-list-comma-newline-after": "always",
+      "selector-no-id": true,
+      "string-quotes": "double",
+      "value-no-vendor-prefix": true
+    }
+  }
+
+  grunt.initConfig({
+    copy: {
+      main: {
+        files: [
+          {
+            src: 'index.html',
+            dest: 'dist/index.html'
+          }
+        ]
+      },
+      assets: {
+        files: [
+          {
+            expand: true,
+            cwd: 'assets/',
+            src: ['**', '!styles/**'],
+            dest: 'dist/'
+          }
+        ],
+      },
+      bower_components: {
+        files: [
+          {
+            expand: true,
+            cwd: 'bower_components/',
+            src: ['**'],
+            dest: 'dist/vendor/'
+          }
+        ],
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      html: {
+        files: ['index.html'],
+        tasks: ['copy:main']
+      },
+      sass: {
+        files: ['main.scss'],
+        tasks: ['sass', 'cssmin']
+      },
+      assets: {
+        files: ['assets/**'],
+        tasks: ['copy:assets', 'imagemin:assets']
+      },
+      bower_components: {
+        files: ['bower_components/**'],
+        tasks: ['copy:bower_components']
+      }
+    },
+
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'dist/main.css': 'main.scss'
+        }
+      }
+    },
+
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'dist',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist',
+          ext: '.min.css'
+        }]
+      }
+    },
+
+    imagemin: {
+      assets: {
+        files: [{
+          expand: true,
+          cwd: 'dist/images',
+          src: ['**/*.{png,jpg,gif,svg, ico}'],
+          dest: 'dist/images'
+        }]
+      }
+    },
+
+    clean: {
+      release: ["dist"]
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: 'dist',
+          hostname: 'localhost',
+          livereload: true
+        }
+      }
+    },
+
+    npmcopy: {
+      dist: {
+        options: {
+          destPrefix: 'dist/vendor'
+        },
+        files: {
+          'js/jquery/jquery.js': 'jquery/dist/jquery.min.js'
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        processors: [
+          require('stylelint')(stylelintConfig)
+        ]
+      },
+      dist: {
+        src: 'assets/styles/*.scss'
+      }
+    },
+
+    html5validate: {
+      src: 'dist/*.html'
+    },
+
+    wkhtmltopdf: {
+      dev: {
+        src: 'dist/*.html',
+        dest: 'dist/',
+        args: [
+          '--print-media-type',
+          '--no-pdf-compression',
+          '--margin-bottom', '0mm',
+          '--margin-right', '0mm',
+          '--margin-left', '0mm',
+          '--margin-top', '0mm'
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-npmcopy');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-html5-validate');
+  grunt.loadNpmTasks('grunt-wkhtmltopdf');
+
+  grunt.registerTask('default', ['postcss', 'copy', 'html5validate', 'sass', 'cssmin', 'imagemin', 'npmcopy', 'wkhtmltopdf']);
+  grunt.registerTask('run', ['clean', 'default', 'connect', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,26 @@
+# Threats: Vermus
+
+Static HTML document for 'Threats: Vermus'.
+
+Requirements:
+
+  * [Node.js](http://nodejs.org/).
+  * [wkhtmltopdf](http://wkhtmltopdf.org/)
+
+To set-up your environment execute:
+
+    $ npm install
+
+To build execute:
+
+    $ npm run build
+
+To test:
+
+    $ npm run start
+
+And open http://localhost:8000 in your browser.
+
+To clean build:
+
+    $ npm run clean
